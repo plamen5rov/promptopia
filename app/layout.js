8518381297e121e1af85bbbf343deb11f3e50a0e
Plamen Petrov <biznetmen@gmail.com>
2023-08-28 10:30:08 +0300

    feat: add DB connection preset

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,19 @@ export const metadata = {
   description: "Discover & share AI prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
           </main>
+        </Provider>
       </body>
     </html>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
